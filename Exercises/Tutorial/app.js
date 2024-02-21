ef29262bc0b5ab19f9dfa82f78b00ce2b52c2d1f
Filipe Fernandes <filipe.fernandes@tqc.local>
2024-02-21 16:58:58 +0000

    lesson 60 HTTP done

--- Exercises/Tutorial/app.js
+++ Exercises/Tutorial/app.js
@@ -1,19 +1,13 @@
-// npm - global command, comes with node
-// npm --version to check version
+const { readFile, writeFile } = require("fs");
 
-// local dependency - use it only in this particular case
-// npm i <packageName>
+console.log("start a first task");
 
-// global dependency - use it in any project
-// npm install -g <packageName>
-
-//package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, create properties, etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
-
-const _ = require("lodash");
-
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+  console.log("completed first task");
+});
+console.log("starting next task");
